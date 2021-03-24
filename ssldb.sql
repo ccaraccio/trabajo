2b721c0f216126ce75049f61158142f8e0f47587
ccaraccio <carloscaraccio@gmail.com>
2021-03-24 14:58:38 -0300

    Se agregaron lineas de creacion de BD y creacion de usuarios

--- ssldb.sql
+++ ssldb.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `ips`
 --
-
+CREATE DATABASE `ssldb`;
+CREATE USER fpuser identified by 'xxxxx';
 DROP TABLE IF EXISTS `ips`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
