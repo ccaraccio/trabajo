abfb6ba115ecda12a019788774b7b4f0ba1cc98a
ccaraccio <carloscaraccio@gmail.com>
2021-03-26 08:27:03 -0300

    Creacion Documentacion respaldatoria
    Documentacion tecnica y funcional

--- ssldb.sql
+++ ssldb.sql
@@ -18,8 +18,7 @@
 --
 -- Table structure for table `ips`
 --
-CREATE DATABASE `ssldb`;
-CREATE USER fpuser identified by 'xxxxx';
+IF NOT EXISTS CREATE DATABASE `ssldb`;
 DROP TABLE IF EXISTS `ips`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
